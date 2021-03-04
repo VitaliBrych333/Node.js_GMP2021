cb5d3e0832d67570447ed639a9aedb578a6d24e6
Vitali Brych <vbrych@lululemon.com>
2021-03-05 02:26:39 +0300

    feat: add solution

---
+++ src/task2/task2.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+const { pipeline } = require('stream');
+const zlib = require('zlib');
+
+const csvFilePath = 'files/nodejs-hw1-ex1.csv';
+const readStream = fs.createReadStream(csvFilePath); 
+
+readStream
+  .pipe(
+    csv({
+      delimiter: [";"],
+      headers: ['book', 'author', 'amount', 'price'],
+      ignoreColumns: /(Amount)/,
+      colParser: {
+        'price': (str) => parseFloat(str.replace(',','.')),
+      }
+    })
+  )
+  .on('data', (data) => {
+    try {
+      const line = JSON.stringify(JSON.parse(data), ['book', 'author', 'price']);
+      console.log('New line: ', line)
+      fs.appendFileSync('output.txt', line + '\n', 'utf8');
+    }
+    catch(err) {
+      console.log('Can not write: ', err);
+    }
+  })
+  .on('error', (err) => {
+    console.log('Can not read: ', err);
+  })
+  .on('end', () => {
+    console.log('End: ');
+  });
+
+pipeline(
+  readStream,
+  zlib.createGzip(),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  }
+);
---
+++ src/task3/task3.1.js
@@ -0,0 +1,23 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+const reverseString = (str) => {
+  let newString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+};
+
+const reverse = () =>
+  rl.question('Please enter a value: ', (val) => {
+    console.log('Reversed value:', reverseString(val));
+    reverse();
+  });
+
+reverse();
